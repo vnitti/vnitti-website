70efcaa76e69e3ffd262fc1f7e6cf82b2e01530f
vnitti <vicentenitti8@gmail.com>
2024-03-27 01:18:16 -0300

    css should be done now

--- script2.js
+++ script2.js
@@ -1,14 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let currentPage = window.location.pathname;
+    let currentPage = window.location.href;
     //let currentPage = "http://127.0.0.1:5502/index.html";
     
     // Get references to the menu buttons
     const menuButtons = document.querySelectorAll(".menu-btn");
-    let anchor = null;
   
     // Check the current page and add the "active" class to the corresponding button
     menuButtons.forEach(button => {
-        anchor = button.querySelector("a");
+        let anchor = button.querySelector("a");
         let anchorHref = anchor.href;
 
         console.log(anchorHref);
@@ -17,6 +16,7 @@ document.addEventListener("DOMContentLoaded", function() {
         if (anchorHref === currentPage) {
             button.classList.add("active-menu-btn");
             anchor.classList.add("active-menu-a");
+            console.log("IT'S THE SAME SHIT");
         };
     });
 });
