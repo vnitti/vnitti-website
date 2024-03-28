bde265938bf5fb4eeed99e4a8b914e0380cb7dc5
vnitti <vicentenitti8@gmail.com>
2024-03-28 13:25:21 -0300

    agregando al script el condicional de agregar index.html al final del currentPage. Si estamos en el Home (sin index.html, ahora debería resaltar el active button tambien

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const currentPage = window.location.href;
 const currentPath = window.location.pathname;
 const pixelsPath = "pixelart.html";
-const currentDomain = currentPage.split("/")[2]; //returns "https://www.vnitti.com"
-const domainString = "https://www.vnitti.com/";
+
+const currentPage = window.location.href;
+//const currentDomain = currentPage.split("/")[2]; //returns "https://www.vnitti.com"
+//const domainString = "https://www.vnitti.com/";
 
 // ===== ONLY FOR PIXELS PAGE =====
 if (currentPath === pixelsPath) {
@@ -26,16 +27,20 @@ if (currentPath === pixelsPath) {
 // ===== GENERAL =====
 document.addEventListener("DOMContentLoaded", ()=> {
     const menuAnchors = document.querySelectorAll(".menu-a");
+
+    if (currentPage.endsWith("/")) {
+        currentPage += "index.html"; // Append "index.html" to the URL
+    };
   
     menuAnchors.forEach(a => {
         const btn = a.querySelector("button");
         const aHref = a.href;
 
-        console.log(domainString);
-        console.log(currentDomain);
-        console.log(aHref);
+        //console.log(domainString);
+        //console.log(currentDomain);
+        //console.log(aHref);
 
-        if (currentPage === aHref || currentPage === domainString) {
+        if (currentPage === aHref) {
             btn.classList.add("active-menu-btn");
             console.log("IT'S THE SAME SHIT");
         };
