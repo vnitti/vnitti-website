4312548ef0a9f4297fcef6de647975ce143cd6f7
vnitti <vicentenitti8@gmail.com>
2024-03-28 11:28:15 -0300

    Listo: todo el css y js general || Falta: aislar el workout generator con su propio css y js || Falta: llenar los html de info real

--- script.js
+++ script.js
@@ -1,25 +1,46 @@
-let galleryImg = document.querySelectorAll('.gallery img');
-let ex = document.querySelector('.ex');
-let navLi = document.querySelectorAll('.nav-ul');
+const currentPage = window.location.href;
+const currentPath = window.location.pathname;
+const pixelsPath = "pixelart.html";
 
-galleryImg.forEach(image => {
-    image.onclick = () => {
-        document.querySelector('.popup-img').style.display = 'block';
-        document.querySelector('.popup-img img').src = image.getAttribute('src');
-    }
+// ===== ONLY FOR PIXELS PAGE =====
+if (currentPath === pixelsPath) {
+
+    console.log("aaa");
+    let galleryImg = document.querySelectorAll('.gallery img');
+    let ex = document.querySelector('.ex');
+
+    galleryImg.forEach(image => {
+        image.onclick = () => {
+            document.querySelector('.popup-img').style.display = 'block';
+            document.querySelector('.popup-img img').src = image.getAttribute('src');
+        }
+    });
+
+    ex.addEventListener('click', () => {
+        document.querySelector('.popup-img').style.display = 'none';
+        console.log("img selected");
+    });
+};
+
+
+// ===== GENERAL =====
+document.addEventListener("DOMContentLoaded", ()=> {
+    const domain = currentPage.split("/")[2]; //returns "https://www.vnitti.com"
+    
+    const menuAnchors = document.querySelectorAll(".menu-a");
+  
+    menuAnchors.forEach(a => {
+        let btn = a.querySelector("button");
+        let aHref = a.href;
+
+        console.log(currentPage);
+
+        if (aHref === currentPage || aHref === domain) {
+            btn.classList.add("active-menu-btn");
+            console.log("IT'S THE SAME SHIT");
+        };
+    });
 });
 
-ex.addEventListener('click', () => {
-    document.querySelector('.popup-img').style.display = 'none';
-    console.log("img selected");
-});
-/*
-navLi.forEach(li => {
-    li.onclick = () => {
-        console.log("1");
-        document.querySelector('. li a class') = 'nav-li-selected';
-        console.log("2");
-    }
-})*/
 
 
--- script2.js
+++ script2.js
@@ -1,21 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
     let currentPage = window.location.href;
-    //let currentPage = "http://127.0.0.1:5502/index.html";
+    let domain = currentPage.split("/")[2];
+    console.log(domain);
     
-    // Get references to the menu buttons
-    const menuButtons = document.querySelectorAll(".menu-btn");
+    const menuAnchors = document.querySelectorAll(".menu-a");
   
-    // Check the current page and add the "active" class to the corresponding button
-    menuButtons.forEach(button => {
-        let anchor = button.querySelector("a");
-        let anchorHref = anchor.href;
+    menuAnchors.forEach(a => {
+        let btn = a.querySelector("button");
+        let aHref = a.href;
 
-        console.log(anchorHref);
         console.log(currentPage);
 
-        if (anchorHref === currentPage) {
-            button.classList.add("active-menu-btn");
-            anchor.classList.add("active-menu-a");
+        if (aHref === currentPage || aHref === domain) {
+            btn.classList.add("active-menu-btn");
             console.log("IT'S THE SAME SHIT");
         };
     });
